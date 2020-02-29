8faab2c0ba43d7766fcc1ea2a884073c62418ec6
Randall Burgess <randallallenburgess@gmail.com>
2020-02-28 17:35:12 -0800

    built server in server.js

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 'use strict';
 
 const express = require('express');
+const expressHandlebars = require('express-handlebars');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
+const routes = require('./controllers/burgers_controllers.js');
+
+app.use(routes);
+
+app.listen(PORT, () =>
+
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
